1c35dff8ea9e123456bbc532213dd93dc2b09eea
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-12-31 22:04:14 +0530

    add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useLayoutEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Terms } from './pages/Terms';
 import { Privacy } from './pages/Privacy';
 import { FAQ } from './pages/FAQ';
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
+import { Footer } from './components/Footer';
+import { ScrollToTop } from './components/ScrollToTop';
+import { WhatsAppButton } from './components/WhatsAppButton';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/faq" element={<FAQ />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+      <Footer />
+      <WhatsAppButton />
     </Router>
   );
 }
